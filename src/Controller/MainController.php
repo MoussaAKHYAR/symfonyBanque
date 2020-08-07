c6218799ab6630a1f7ccd051bffa66de98ede927
MoussaAKHYAR <amrane93.mb@gmail.com>
2020-08-07 13:59:38 +0000

    gestion utilisateur

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     /**
-     * @Route("/", name="main")
+     * @Route("/", name="app_home")
      */
     public function index()
     {
